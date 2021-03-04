01bac1af0f624020a6f47b7b6d3f123f78c34c50
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-03-04 14:44:20 +0100

    primer commit

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+interface componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: componente [] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'alert-circle-outline',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+
+  ];
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
