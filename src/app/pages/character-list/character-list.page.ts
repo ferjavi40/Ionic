bea98df59ef8afda0f5178d9de9c3f9690bdad6c
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-03-08 12:14:22 +0100

    terminado

--- src/app/pages/character-list/character-list.page.ts
+++ src/app/pages/character-list/character-list.page.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit,OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { MortyDataService } from '../../services/morty-data.service';
-import { AllCharactersInterface } from '../../interfaces/all-characters';
 import { MontyInterface } from '../../interfaces/monty-interface';
 
 
@@ -16,21 +15,25 @@ import { MontyInterface } from '../../interfaces/monty-interface';
 })
 export class CharacterListPage implements OnInit, OnDestroy {
 
-  public personajes: MontyInterface []  = [];
-  private personajes$: Subscription= null;
+  public personajes: MontyInterface[] = [];
+  private personajes$: Subscription = null;
 
-  constructor( private _mortyService: MortyDataService) { }
+  constructor(private _mortyService: MortyDataService) { }
 
   ngOnInit() {
-    this.personajes$= this._mortyService.getAllCharacter()
-          .subscribe((personajes)=>{
-            console.log(personajes);
-            this.personajes=personajes;
-          });
+    this.personajes$ = this._mortyService.getAllCharacter()
+      .subscribe((personajes) => {
+        console.log(personajes);
+        this.personajes = personajes;
+      });
 
   }
 
-  ngOnDestroy(): void{
-    if(this.personajes$) this.personajes$.unsubscribe()
+  ngOnDestroy(): void {
+    if (this.personajes$) this.personajes$.unsubscribe()
   }
+
+
+
 }
+
